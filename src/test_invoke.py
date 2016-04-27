6fc4f59b3040a514304ec5d341d7ccade6462d5d
Karthikeyan Vasuki Balasubramaniam <kvasukib@ucsd.edu>
2016-04-27 00:43:52 -0700

    Adding delete

--- src/test_invoke.py
+++ src/test_invoke.py
@@ -1,7 +1,7 @@
 from sign_invoke import invokeLambda
 import json
 
-(output, ret_code)=invokeLambda('HelloWorld')
+(output, ret_code)=invokeLambda('Helloworldv1')
 output = json.loads(output)
 if output['output'] == "Hello World" and ret_code == 200:
 	print "Invocation Success"
